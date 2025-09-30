29fd510f89f2f6386f4aec118ab2161403f2508a
tpaau-17DB <tpaau-17DB@tutamail.com>
2025-09-30 21:36:43 +0200

    Initial commit.

--- config/tpaau-17DB/scripts/logs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if (( UTILS_SOURCED != 1 )); then source ~/.config/tpaau-17DB/scripts/lib/utils.sh; fi
-if (( LOGGER_SOURCED != 1 )); then source ~/.config/tpaau-17DB/scripts/lib/logger.sh; fi
-if (( PATHS_SOURCED != 1 )); then source ~/.config/tpaau-17DB/scripts/lib/paths.sh; fi
-
-log_info "Tailing log file '$LOG_FILE'"
-view_logs "$LOG_FILE"
-exit $?
--- config/tpaau-17DB/scripts/run-wlogout.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-log_debug "Launching wlogout"
-
-cd $HOME/.config/wlogout/
-pidof wlogout || wlogout -b 4 --protocol layer-shell
